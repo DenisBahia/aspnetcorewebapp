8c857ab1278023afed3293b1ff322d83bee66405
DenisBahia <45142971+DenisBahia@users.noreply.github.com>
2019-03-06 07:57:48 -0300

    Initial commit

---
+++ aspnetcorewebapp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using aspnetcorewebapp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnetcorewebapp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        [HttpPost]
+        public IActionResult Index(HomeModel form)
+        {
+            CarregaDados();
+            if (form.id != 0)
+            {
+                HomeModel item = new HomeModel();
+                item.id = form.id;
+                item.Nome = form.Nome;
+                ViewBag.lista.Add(item);
+            }
+
+            return View();
+        }
+
+        public void CarregaDados()
+        {
+            HomeModel objHomeModel = new HomeModel();
+            ViewBag.lista = objHomeModel.GetAll();
+        }
+
+    }
+}
---
+++ aspnetcorewebapp/Models/HomeModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnetcorewebapp.Models
+{
+    public class HomeModel
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+
+        public List<HomeModel> GetAll()
+        {
+            HomeModel item;
+            List<HomeModel> lista = new List<HomeModel>();
+
+            item = new HomeModel();
+            item.id = 1;
+            item.Nome = "Denis";
+            lista.Add(item);
+
+            item = new HomeModel();
+            item.id = 2;
+            item.Nome = "Aluno";
+            lista.Add(item);
+
+            return lista;
+
+        }
+
+    }
+}
